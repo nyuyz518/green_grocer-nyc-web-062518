49279e20c6354ba015fb6e1e18504d9825f3cb8d
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-06 02:38:43 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -18,6 +18,10 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     name = coupon[:item]
     if cart[name] && cart[name][:count] >= coupon[:num]
+      if cart["#{name} W/COUPON"]
+        cart["#{name} W/COUPON"][:count] +=1 
+      else 
+        cart["#{name} W/COUPON"] = {:count =>1, 
     
 end
 
