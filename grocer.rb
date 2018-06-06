09541ccc61c934c26e82bcced34c979221567102
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-06 02:32:34 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,12 @@
 def consolidate_cart(cart)
   # code here
   cart.each_with_object({}) do |item, hash|
-    item.each do |type, attributes
+    item.each do |type, attributes|
+      if hash[type]
+        attributes[:count] +=1 
+      else
+        attributes[:count] = 1 
+        
 end
 
 def apply_coupons(cart, coupons)
